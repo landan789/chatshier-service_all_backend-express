a3e7d2b5add0d334acd766f4d20650bb1ff19833
TED <hwanghsin@9thflr.com>
2017-11-30 16:44:45 +0800

    登入時寫入cookie 登出cookie就刪除掉

--- public/javascripts/firebaseAuth.js
+++ public/javascripts/firebaseAuth.js
@@ -1,27 +1,41 @@
+var serviceUrl = location.host;
+var domain = serviceUrl.replace(/^[\w\-]+\./i, '.').replace(/\:\d+$/i, '');
+
 // Initialize Firebase
 firebase.initializeApp(config);
 const auth = firebase.auth();
 const database = firebase.database();
 // log in status
-if(window.location.pathname === '/login' || window.location.pathname === '/signup'){
-  auth.onAuthStateChanged(user => {
-    if(user){
-      window.location = '/chat';
+auth.onAuthStateChanged(user => {
+    if (user) {
+        getProfile();
+        if (location.pathname === '/login' || location.pathname === '/signup') {
+            location = '/chat';
+        }
+    } else if (!user && (location.pathname === '/login' || location.pathname === '/signup')) {
+        document.cookie = "name=; email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; " + "domain=" + domain;
+        console.log('need to sign in');
     } else {
-      console.log('need to sign in');
+        location = '/login';
+        console.log('need to sign in');
     }
-  });
-} else {
-  auth.onAuthStateChanged(user => {
-    if(!user){
-      window.location = '/login';
-    }
-  });
-}
+});
 // functions
-function logout(){
-  auth.signOut()
-  .then(response => {
-    window.location = '/login';
-  })
+function logout() {
+    auth.signOut()
+        .then(response => {
+            location = '/login';
+        })
+}
+
+function getProfile() {
+    let uid = auth.currentUser.uid;
+    console.log(uid);
+    database.ref('users/' + uid).once('value', data => {
+        let profile = data.val();
+        let name = profile.name;
+        let email = profile.email;
+        document.cookie = "name=" + name + "email=" + email + ";domain=" + domain;
+        console.log(name);
+    });
 }
