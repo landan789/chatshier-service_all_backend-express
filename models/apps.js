0651e085cf890c61c1748a159774e7b9f9b0c7e8
HTH <9thflr.tzuhsinhsu@gmail.com>
2017-12-29 09:45:30 +0000

    Merged in feature/api (pull request #81)
    Feature/api
    
    * [feature/api] [ API 錯誤訊息設定檔案 ]
    
    * [feature/api] [ /api/userid/....  根據使用者 id 取出 apps 資料 ]

--- models/apps.js
+++ models/apps.js
@@ -8,6 +8,7 @@ apps.getAll = (callback) => {
     });
 }
 
+
 apps.getById = (appId, callback) => {
     admin.database().ref('apps/' + appId).once('value', snap => {
         var app = snap.val();
@@ -15,5 +16,78 @@ apps.getById = (appId, callback) => {
     });
 }
 
+apps.getAppsByUserId = (userId, callback) => {
+    var p = new Promise((resolve, reject) => {
+        resolve();
+    });
+
+    p.then(() => {
+        return new Promise((resolve, reject) => {
+            admin.database().ref('users/' + userId).once('value', snap => {
+                var appIds = snap.val();
+                resolve(appIds);
+            });
+        });
+    }).then((data) => {
+        var appIds = data;
+        return new Promise((resolve, reject) => {
+            this.getAppsByAppIds(appIds, () => {
+
+            });
+        });
+    });
+
+
+
+}
+
+
+apps.getAppsByAppIds = (appIds, callback) => {
+    var a = appIds;
+
+    var apps = [];
+    next(0, callback);
+
+
+    function next(i, cb) {
+        var p = new Promise((resolve, reject) => {
+            resolve();
+        });
+
+        p.then(() => {
+            return new Promise((resolve, reject) => {
+                console.log(appIds);
+                if (i >= appIds.length) {
+                    reject(apps);
+                    return;
+                }
+                resolve();
+
+            });
+        }).then(() => {
+            return new Promise((resolve, reject) => {
+
+                var appId = appIds[i];
+                admin.database().ref('apps/' + appId).once('value', snap => {
+                    var app = snap.val();
+                    if (null === app || undefined === app || '' === app) {
+                        resolve(apps);
+                        return;
+                    }
+
+                    apps.push(app);
+                    resolve(apps);
+                });
+            });
+        }).then((data) => {
+            var apps = data;
+            next(i + 1, cb);
+        }).catch((data) => {
+            cb(data);
+        });
+
+    }
+
+}
 
 module.exports = apps;
--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,67 @@
+var API_ERROR = require('../config/apiError');
 var express = require('express');
 var admin = require('firebase-admin');
+var users = require('../models/users');
+var apps = require('../models/apps');
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
-	admin.database().ref('users').once('value', snap => {
-        let Data = snap.val();
-        res.send(Data);
+router.get('/apps/appid/:appid', (req, res, next) => {
+
+
+});
+
+router.get('/apps/userid/:userid', (req, res, next) => {
+
+
+    var p = new Promise((resolve, reject) => {
+        resolve();
+    });
+
+    p.then(() => {
+        return new Promise((resolve, reject) => {
+            var userId = req.params.userid;
+            if ('' === userId || null === userId) {
+                reject(API_ERROR.ERROR_USERID_NOT_EXISTS);
+                return;
+            }
+            users.getUser(userId, (data) => {
+                if (null === data) {
+                    reject(API_ERROR.ERROR_USER_NOT_EXISTS);
+                    return;
+                }
+
+                resolve(data);
+            });
+        });
+    }).then((data) => {
+        var appIds = data.app_ids;
+
+        return new Promise((resolve, reject) => {
+            apps.getAppsByAppIds(appIds, (data) => {
+                var apps = data;
+                if (null === apps || '' === apps || undefined === apps) {
+                    reject();
+                }
+
+                resolve(apps);
+            });
+        });
+
+    }).then((apps) => {
+        var json = {
+            "status": 1,
+            "data": apps
+        };
+        res.status(200).json(json);
+    }).catch((ERR) => {
+        var json = {
+            "status": 0,
+            "mgs": ERR.MSG,
+            "code": ERR.CODE
+        };
+        res.status(403).json(json);
     });
 });
 
-router.get('/:id', (req, res, next) => {
-	var id = req.params.id;
-	admin.database().ref('users/' + id).once('value', snap => {
-        let Data = snap.val();
-        res.send(Data);
-    });
-});
 
 module.exports = router;
