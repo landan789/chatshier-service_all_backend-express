b5973aca7be7e3fe5ef3aa3511b1a2258fcf2962
HTH <9thflr.tzuhsinhsu@gmail.com>
2018-01-30 07:44:35 +0000

    Merged in feature/cipher (pull request #230)
    [feature/cipher] [更新 加密 函式 命名]

--- helpers/cipher.js
+++ helpers/cipher.js
@@ -1,23 +1,22 @@
 module.exports = (function() {
     const crypto = require('crypto');
-    const SECRET_KEY = 'chatshier777';
-    const ALGORITHM = 'sha1';
+    const SECRET = require('../config/secret');
 
-    function ChatshierCipher() {}
+    function Cipher() {}
 
     /**
      * 產生一組不可逆的 hash key
      *
      * @param {string} content
      */
-    ChatshierCipher.prototype.createHashKey = function(content) {
+    Cipher.prototype.createHashKey = function(content) {
         if (!content) {
             return content;
         }
-        let sum = crypto.createHmac(ALGORITHM, SECRET_KEY);
+        let sum = crypto.createHmac(SECRET.ALGORITHM, SECRET.APP_MESSAGE_KEY);
         sum.write(content);
         return sum.digest('hex');
     };
 
-    return new ChatshierCipher();
+    return new Cipher();
 })();
