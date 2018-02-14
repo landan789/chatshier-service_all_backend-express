ab96e001ce4d172ba2c31119dd073c6fb5ca0eaf
HTH <9thflr.tzuhsinhsu@gmail.com>
2018-02-14 12:34:57 +0000

    Merged in feature/schedules (pull request #341)
    [feature/schedule] [排程調整]

--- schedules/index.js
+++ schedules/index.js
@@ -1,136 +1,79 @@
-var schedule = require('node-schedule');
-const line = require('@line/bot-sdk');
-var admin = require('firebase-admin');
-var serviceAccount = require('../config/firebase-adminsdk');
-var databaseURL = require('../config/firebase_admin_database_url');
+let schedule = require('node-schedule');
+let line = require('@line/bot-sdk');
+let admin = require('firebase-admin');
+let serviceAccount = require('../config/firebase-adminsdk');
+let databaseURL = require('../config/firebase_admin_database_url');
+let timer = require('../helpers/timer');
+
 const API_ERROR = require('../config/api_error');
 
-var agents = require('../models/agents');
-var appsComposes = require('../models/apps_composes');
-var appsAutorepliesMdl = require('../models/apps_autoreplies');
-var linetemplate = require('../models/linetemplate');
-var chats = require('../models/chats');
-var appsKeywordrepliesMdl = require('../models/apps_keywordreplies');
-var users = require('../models/users');
-
-var apiModel = require('../models/apiai');
-var utility = require('../helpers/utility');
-var webhookMdl = require('../models/webhooks');
-var appsMdl = require('../models/apps');
-var appsChatroomsMessagesMdl = require('../models/apps_chatrooms_messages');
-var appsChatrooms = require('../models/apps_chatrooms');
-var appsMessengersMdl = require('../models/apps_messagers');
-var groupsMdl = require('../models/groups');
-var appsMessagesMdl = require('../models/apps_messages');
-var appsTemplatesMdl = require('../models/apps_templates');
-
+const SYSTEM = 'SYSTEM';
+const CHATSHIER = 'CHATSHIER';
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: databaseURL.url
 });
-var times = [];
 
-for (let i = 0; i < 60; i++) {
-    times.push(i);
-}
-
-var rule1 = new schedule.RecurrenceRule();
-rule1.minute = times;
-var job1 = schedule.scheduleJob(rule1, function() {
-    var timeInMs = ISODateTimeString(new Date());
-    console.log('running');
-    var proceed = new Promise((resolve, reject) => {
-        resolve();
-    });
-    proceed.then(() => {
-        return new Promise((resolve, reject) => {
-            admin.database().ref('apps').once('value', (snap) => {
-                let apps = snap.val();
-                if (!apps) {
-                    reject(API_ERROR.APP_FAILED_TO_FIND);
-                    return;
-                }
-                resolve(apps);
-            });
-        });
-    }).then((apps) => {
-        let composesPromises = [];
-        for (let appId in apps) {
-            let composes = apps[appId].composes === undefined ? '' : apps[appId].composes;
-            let messagers = Object.keys(apps[appId].messagers === undefined ? '' : apps[appId].messagers);
+let job1 = schedule.scheduleJob('0 * * * * *', () => {
+    let nowUnixTime = Date.now();
+    console.log('[start] [' + nowUnixTime + '] schedules/index.js is starting ... ');
+    Promise.resolve().then(() => {
+        return admin.database().ref('apps').once('value');
+    }).then((snap) => {
+        let apps = snap.val();
+        return Promise.all(Object.keys(apps).map((appId) => {
+            let app = apps[appId];
+            if (CHATSHIER === app.type) {
+                return Promise.resolve();
+            }
             let messages = [];
-            for (let composesId in composes) {
-                let time = ISODateTimeString(composes[composesId].time);
-                let status = composes[composesId].status;
-                if (1 === status) {
-                    if (time.match(timeInMs)) {
-                        if (!composes[composesId].text) {
-                            continue;
-                        }
-                        let message = {
-                            'type': composes[composesId].type,
-                            'text': composes[composesId].text
-                        };
-                        messages.push(message);
-                    }
+            let composes = app.composes === undefined ? '' : app.composes;
+            for (let composeId in composes) {
+                if (composes[composeId].text &&
+                    1 === composes[composeId].status &&
+                    timer.minutedUnixTime(nowUnixTime) === timer.minutedUnixTime(composes[composeId].time)) {
+                    let message = {
+                        type: composes[composeId].type,
+                        text: composes[composeId].text
+                    };
+                    messages.push(message);
                 }
-            }
-            if (!messages.length) {
-                continue;
-            }
-            let promise = proceed.then(() => {
-                var lineConfig = {
-                    channelSecret: apps[appId].secret,
-                    channelAccessToken: apps[appId].token1
-                };
-                var lineBot = new line.Client(lineConfig);
-                return lineBot.multicast(messagers, messages);
-            }).then(() => {
-                let asyncTask = [];
-                for (let i in messagers) {
-                    let messagersId = [];
-                    messagersId = messagers[i];
-                    asyncTask.push(admin.database().ref('apps/' + appId + '/messagers/' + messagersId).once('value').then((snap) => {
-                        let messagersInfo = snap.val();
-                        let chatroomId = messagersInfo.chatroom_id;
-                        return chatroomId;
-                        return Promise.all(asyncTask);
-                    }).then((chatroomId) => {
-                        let messageInfo = {};
-                        let updateMessages = [];
-                        for (let j in messages) {
-                            messageInfo = {
-                                from: 'SYSTEM',
-                                messager_id: '',
-                                name: 'agent',
-                                text: messages[j].text,
-                                time: Date.now()
-                            };
-                            updateMessages.push(messageInfo);
-                            admin.database().ref('apps/' + appId + '/chatrooms/' + chatroomId + '/messages').push().then((ref) => {
-                                var messageId = ref.key;
-                                return admin.database().ref('apps/' + appId + '/chatrooms/' + chatroomId + '/messages/' + messageId).update(updateMessages[j]);
-                            });
-                        }
-                    }));
-                }
-            });
-            composesPromises.push(promise);
-        }
+            };
+            let messagers = app.messagers || {};
+            let config = {
+                channelSecret: app.secret,
+                channelAccessToken: app.token1
+            };
+            let lineBot = new line.Client(config);
 
-        return Promise.all(composesPromises);
+            // 沒有訊息對象 或 沒有群發訊息 就不做處理
+            if (0 === Object.keys(messagers).length || 0 === messages.length) {
+                return Promise.resolve(null);
+            };
+
+            return Promise.all(messages.map((message) => {
+                return lineBot.multicast(messagers, message);
+            })).then(() => {
+                return Promise.all(Object.keys(messagers).map((messagerId) => {
+                    let messager = messagers[messagerId];
+                    let chatroomId = messager.chatroom_id;
+
+                    return Promise.all(messages.map((message) => {
+                        let _message = {
+                            from: SYSTEM,
+                            messager_id: messagerId,
+                            createdTime: Date.now()
+                        };
+                        message = Object.assign(message, _message);
+                        return admin.database().ref('apps/' + appId + '/chatrooms/' + chatroomId + '/messages').push(message);
+                    }));
+                }));
+            });
+        }));
+    }).then(() => {
+        console.log('[finish] [' + Date.now() + '] schedules/index.js is finishing ... ');
     }).catch((err) => {
+        console.log('[fail] [' + Date.now() + '] schedules/index.js is failing ... ');
         console.error(err);
     });
 });
-// all composes
-function ISODateTimeString(d) {
-    d = new Date(d);
-
-    function pad(n) { return n < 10 ? '0' + n : n }
-    return d.getFullYear() + '-' +
-        pad(d.getMonth() + 1) + '-' +
-        pad(d.getDate()) + 'T' +
-        pad(d.getHours()) + ':' +
-        pad(d.getMinutes());
-}
