e7fbeb5f0aa26274591e4c807dc88dae700d4b5b
PMH <9thflr.minghepan@gmail.com>
2018-03-07 05:59:04 +0000

    Merged in feature/fix_logout_bug (pull request #458)
    修正加入 google client auth 後，登出頁面畫面停止問題

--- public/js/logout.js
+++ public/js/logout.js
@@ -16,10 +16,14 @@
         var url = window.googleCalendarHelper.configJsonUrl;
         return window.googleClientHelper.init(url);
     }).then(function(gAuth) {
-        if (!gAuth.isSignedIn.get()) {
+        if (!(gAuth && gAuth.isSignedIn.get())) {
             return;
         }
         return gAuth.signOut();
+    }).catch(function(err) {
+        console.error(err);
+        // catch google auth 登出
+        // 有無問題都繼續往下執行
     }).then(function() {
         return logout();
     }).then(function() {
