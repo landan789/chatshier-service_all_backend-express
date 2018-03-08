f6ab12f5cf4d7445406179c075485035c4c458f4
PMH <9thflr.minghepan@gmail.com>
2018-03-08 06:10:11 +0000

    Merged in feature/submodule_build (pull request #478)
    新增建置 react 子模組的 shell script
    
    * remove(files): 移除 react build files
    
    * modify .gitignore
    
    * add(script): add build script for submodule react.chatshier
    
    * modify(package.json): add script for prestart

---
+++ bin/build-react.sh
@@ -0,0 +1,36 @@
+git submodule init
+
+git submodule update
+
+# replace config file content and rename to react.chatshier
+content="$(sed -e "s/window.config/const firebaseConfig/g" public/config/firebaseConfig.js)"
+exportStr=" export default firebaseConfig;"
+content="$content$exportStr"
+rm -f react.chatshier/src/config/firebase.js
+echo $content >> react.chatshier/src/config/firebase.js
+
+content="$(sed -e "s/window.urlConfig/const urlConfig/g" public/config/url-config.js)"
+exportStr=" export default urlConfig;"
+content="$content$exportStr"
+rm -f react.chatshier/src/config/url.js
+echo $content >> react.chatshier/src/config/url.js
+
+# enter submodule
+cd react.chatshier
+
+git pull origin develop
+
+npm update
+
+npm run build
+
+cd ..
+# leave submodule
+
+rm -Rf public/static
+
+rm -f views/react.ejs
+
+cp react.chatshier/build/index.html views/react.ejs
+
+cp -R react.chatshier/build/static public/static
