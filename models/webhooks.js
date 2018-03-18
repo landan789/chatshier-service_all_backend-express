f867381a6f2d71edaeef243fd584fbd37cb59d41
HTH <9thflr.tzuhsinhsu@gmail.com>
2018-03-19 00:18:15 +0800

    [feature/webhook] [> find]

--- models/webhooks.js
+++ models/webhooks.js
@@ -1,7 +1,7 @@
 var admin = require("firebase-admin"); //firebase admin SDK
 var webhooks = {};
 
-webhooks.findByWebhookId = (webhookId, callback) => {
+webhooks.find = (webhookId, callback) => {
     admin.database().ref('webhooks/' + webhookId).once('value', snap => {
         var webhook = snap.val();
         var webhookId = Object.keys(webhook)[0];
@@ -14,19 +14,5 @@ webhooks.findByWebhookId = (webhookId, callback) => {
         callback(webhooks);
     });
 }
-
-webhooks.findAppIdByWebhookId = (webhookId, callback) => {
-    var procced = Promise.resolve();
-
-    procced.then(() => {
-        return admin.database().ref('webhooks/' + webhookId).once('value');
-    }).then((snap) => {
-        var webhook = snap.val();
-        var appId = webhook.app_id;
-        callback(appId);
-    }).catch(() => {
-        callback(null);
-    });
-};
 
 module.exports = webhooks;
