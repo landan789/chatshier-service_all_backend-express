95074715c69f0ecf0389eb5e69ad547a6380affd
WCT <9thflrchihting@gmail.com>
2018-03-29 16:23:17 +0800

    feature/greetings_time
    insert time debug

--- models/apps_chatrooms_messages.js
+++ models/apps_chatrooms_messages.js
@@ -138,7 +138,7 @@ module.exports = (function() {
                     isDeleted: false,
                     from: message.from,
                     messager_id: message.messager_id,
-                    text: message.text || (message.altText ? message.altText + '\n' : '') + '請至智慧手機上確認訊息內容。',
+                    text: message.text || (message.altText ? message.altText  + '請至智慧手機上確認訊息內容。'+'\n' : ''),
                     time: Date.now(),
                     type: message.type,
                     src: message.src || ''
