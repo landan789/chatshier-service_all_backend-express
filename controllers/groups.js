ebc1594749c0803229dbd23522949af66c3ff941
LAZ <9thflr.lianzheng@gmail.com>
2018-04-09 13:52:10 +0800

    fix(bug): 修復內部群組名稱修改，但聊天室名稱無修正的問題

--- controllers/groups.js
+++ controllers/groups.js
@@ -240,6 +240,31 @@ module.exports = (function() {
                 });
             });
         }).then((groups) => {
+            let putApp = {
+                name: 'Chatshier - ' + putGroup.name
+            };
+            return new Promise((resolve, reject) => {
+                groupsMdl.findAppIds(groupId, userId, (appIds) => {
+                    appsMdl.find(appIds, null, (apps) => {
+                        if (!apps) {
+                            reject(API_ERROR.APP_FAILED_TO_FIND);
+                            return;
+                        }
+                        for (let appId in apps) {
+                            let app = apps[appId];
+                            if (CHATSHIER === app.type) { // 更新 屬於 內部群組 的 app 名稱
+                                appsMdl.update(appId, putApp).then((app) => {
+                                    if (!app) {
+                                        reject(API_ERROR.APP_FAILED_TO_UPDATE);
+                                    }
+                                });
+                            }
+                        }
+                        resolve(groups);
+                    });
+                });
+            });
+        }).then((groups) => {
             let json = {
                 status: 1,
                 msg: API_SUCCESS.DATA_SUCCEEDED_TO_UPDATE.MSG,
