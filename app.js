9e83a057b375e8b72c2baa1ffc12068d721e301f
HTH <9thflr.tzuhsinhsu@gmail.com>
2018-04-12 19:20:20 +0800

    [feature/www_router]
    [fixed the router error]

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let cookieParser = require('cookie-parser');
 let cors = require('cors');
 
 let jwtHlp = require('./helpers/jwt');
+let routerHlp = require('./helpers/router');
 let index = require('./routes/index');
 let apiDatabase = require('./routes/api_database');
 let apiSign = require('./routes/api_sign');
@@ -22,11 +23,12 @@ app.use(cookieParser());
 
 // API JWT 權限驗證
 app.use('/api/*/users/:userid', jwtHlp.authenticate('HEADER', null));
-// API
 app.use('/api/database', apiDatabase);
 app.use('/api/sign', apiSign);
+app.use('/api/*', routerHlp.requestNotExistentApi);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
+app.use('/', routerHlp.requestNotExistentPath);
 
 module.exports = app;
