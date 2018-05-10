e25cf848590eb0ff191a6ca54df039cfc6bab40f
TED <hwanghsin@9thflr.com>
2018-05-10 17:54:18 +0800

    re-construct template controller

--- controllers/apps_templates.js
+++ controllers/apps_templates.js
@@ -19,20 +19,19 @@ module.exports = (function() {
         }
 
         getAll(req, res, next) {
-            // TODO
             return this.appsRequestVerify(req).then((checkedAppIds) => {
                 let appIds = checkedAppIds;
-                return appsTemplatesMdl.find(appIds).then((appsTemplates) => {
-                    if (!appsTemplates) {
-                        return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_FIND);
-                    }
-                    return appsTemplates;
-                });
-            }).then((data) => {
+                return appsTemplatesMdl.find(appIds);
+            }).then((appsTemplates) => {
+                if (!appsTemplates) {
+                    return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_FIND);
+                }
+                return appsTemplates;
+            }).then((appsTemplates) => {
                 let json = {
                     status: 1,
                     msg: API_SUCCESS.DATA_SUCCEEDED_TO_FIND.MSG,
-                    data: data
+                    data: appsTemplates || {}
                 };
                 res.status(200).json(json);
             }).catch((ERROR) => {
@@ -50,13 +49,12 @@ module.exports = (function() {
             let templateId = req.params.templateid;
 
             return this.appsRequestVerify(req).then(() => {
-                // TODO
-                return appsTemplatesMdl.find(appId, templateId).then((appsTemplates) => {
-                    if (!appsTemplates) {
-                        return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_FIND);
-                    }
-                    return appsTemplates;
-                });
+                return appsTemplatesMdl.find(appId, templateId);
+            }).then((appsTemplates) => {
+                if (!appsTemplates) {
+                    return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_FIND);
+                }
+                return appsTemplates;
             }).then((appsTemplates) => {
                 let json = {
                     status: 1,
@@ -87,32 +85,30 @@ module.exports = (function() {
                 template: template
             };
             return this.appsRequestVerify(req).then(() => {
-                // TODO
-                return appsTemplatesMdl.insert(appId, postTemplate).then((appsTemplates) => {
-                    if (!appsTemplates) {
-                        return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_INSERT);
-                    }
-                    return appsTemplates;
-                });
+                return appsTemplatesMdl.insert(appId, postTemplate);
             }).then((appsTemplates) => {
-                return Promise.resolve().then(() => {
-                    let templateId = Object.keys(appsTemplates[appId].templates)[0];
-                    if (appsTemplates[appId].templates[templateId].template.thumbnailImageUrl) {
-                        let fromPathArray = (appsTemplates[appId].templates[templateId].template.thumbnailImageUrl).split('/');
-                        let fromPath = `/temp/${fromPathArray[5]}`;
-                        let toPath = `/apps/${appId}/template/${templateId}/src/${fromPathArray[5]}`;
-                        return storageHlp.filesMoveV2(fromPath, toPath);
-                    }
-
-                    return Promise.all(Object.keys(appsTemplates[appId].templates[templateId].template.columns).map((img) => {
-                        let fromPathArray = (appsTemplates[appId].templates[templateId].template.columns[img].thumbnailImageUrl).split('/');
-                        let fromPath = `/temp/${fromPathArray[5]}`;
-                        let toPath = `/apps/${appId}/template/${templateId}/src/${fromPathArray[5]}`;
-                        return storageHlp.filesMoveV2(fromPath, toPath);
-                    }));
-                }).then(() => {
+                if (!appsTemplates) {
+                    return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_INSERT);
+                }
+                return appsTemplates;
+            }).then((appsTemplates) => {
+                let templateId = Object.keys(appsTemplates[appId].templates)[0];
+                if (appsTemplates[appId].templates[templateId].template.thumbnailImageUrl) {
+                    let fromPathArray = (appsTemplates[appId].templates[templateId].template.thumbnailImageUrl).split('/');
+                    let src = fromPathArray[5];
+                    let fromPath = `/temp/${src}`;
+                    let toPath = `/apps/${appId}/template/${templateId}/src/${src}`;
+                    storageHlp.filesMoveV2(fromPath, toPath);
                     return appsTemplates;
-                });
+                }
+                return Promise.all(Object.keys(appsTemplates[appId].templates[templateId].template.columns).map((img) => {
+                    let fromPathArray = (appsTemplates[appId].templates[templateId].template.columns[img].thumbnailImageUrl).split('/');
+                    let src = fromPathArray[5];
+                    let fromPath = `/temp/${src}`;
+                    let toPath = `/apps/${appId}/template/${templateId}/src/${src}`;
+                    storageHlp.filesMoveV2(fromPath, toPath);
+                    return appsTemplates;
+                }));
             }).then((appsTemplates) => {
                 let json = {
                     status: 1,
@@ -149,23 +145,17 @@ module.exports = (function() {
                 if (!templateId) {
                     return Promise.reject(API_ERROR.TEMPLATEID_WAS_EMPTY);
                 }
-
-                return new Promise((resolve, reject) => {
-                    appsTemplatesMdl.update(appId, templateId, putTemplateData, (appsTemplate) => {
-                        if (!appsTemplate) {
-                            reject(API_ERROR.APP_TEMPLATE_FAILED_TO_UPDATE);
-                            return;
-                        }
-                        resolve(appsTemplate);
-                    });
-                });
+                return appsTemplatesMdl.update(appId, templateId, putTemplateData);
             }).then((appsTemplate) => {
-                // TODO
-                let result = appsTemplate !== undefined ? appsTemplate : {};
+                if (!appsTemplate) {
+                    return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_UPDATE);
+                }
+                return Promise.resolve(appsTemplate);
+            }).then((appsTemplate) => {
                 let json = {
                     status: 1,
                     msg: API_SUCCESS.DATA_SUCCEEDED_TO_UPDATE,
-                    data: result
+                    data: appsTemplate || {}
                 };
                 res.status(200).json(json);
             }).catch((ERR) => {
@@ -187,23 +177,17 @@ module.exports = (function() {
                 if (!templateId) {
                     return Promise.reject(API_ERROR.TEMPLATEID_WAS_EMPTY);
                 };
-                return new Promise((resolve, reject) => {
-                    appsTemplatesMdl.remove(appId, templateId, (result) => {
-                        // TODO
-                        if (!result) {
-                            reject(API_ERROR.APP_TEMPLATE_FAILED_TO_REMOVE);
-                            return;
-                        }
-                        resolve(result);
-                    });
-                });
+                return appsTemplatesMdl.remove(appId, templateId);
             }).then((appsTemplates) => {
-                // TODO
-                let result = appsTemplates !== undefined ? appsTemplates : {};
+                if (!appsTemplates) {
+                    return Promise.reject(API_ERROR.APP_TEMPLATE_FAILED_TO_REMOVE);
+                }
+                return Promise.resolve(appsTemplates);
+            }).then((appsTemplates) => {
                 let json = {
                     status: 1,
                     msg: API_SUCCESS.DATA_SUCCEEDED_TO_REMOVE,
-                    data: result
+                    data: appsTemplates || {}
                 };
                 res.status(200).json(json);
             }).catch((ERR) => {
