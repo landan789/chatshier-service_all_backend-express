5ed2878449b845d2918459837cbfa5d095d6e71c
HTH <9thflr.tzuhsinhsu@gmail.com>
2018-07-30 05:59:58 +0800

    [feature/config]

--- public/config/url-config-dev.js
+++ public/config/url-config-dev.js
@@ -1,6 +1,6 @@
 /// <reference path='../../typings/client/index.d.ts' />
 
-window.urlConfig = {
+window.URL = {
     WWW: '',
     PORT: '',
     WEBHOOK: 'https://service.dev.chatshier.com/webhook',
